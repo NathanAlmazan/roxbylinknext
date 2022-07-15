f9500d1fa34f86b5f3b4ce0b3229d4f63d0c5ae3
NathanAlmazan <nathanael.almazan@gmail.com>
2022-07-16 03:01:31 +0800

    fixed+bug

--- src/components/Sections/Event/EventTable.tsx
+++ src/components/Sections/Event/EventTable.tsx
@@ -13,7 +13,7 @@ import Typography from '@mui/material/Typography';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
-import TextField from '@mui/material/Textfield';
+import TextField from '@mui/material/TextField';
 
 // types 
 import { Event } from 'src/types/event';
